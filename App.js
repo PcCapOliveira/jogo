6d7f66c45548abdb86761f61919eeb85acc14f11
Paulo Cesar <paulo_chevy@hotmail.com>
2023-10-02 18:36:00 -0300

    setjogador

--- App.js
+++ App.js
@@ -1,10 +1,207 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+import { useState } from "react";
+
+import Caixa from "./Caixa";
 
 export default function App() {
+  const [jogador, setJogador] = useState(1);
+  const [tabuleiro, setTabuleiro] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [vencedor, setVencedor] = useState(null);
+
+  /* posições ganhadoras
+
+    1 = 2 = 3
+    4 = 5 = 6
+    7 = 9 = 9
+
+    1 = 4 = 7
+    2 = 5 = 8
+    3 = 6 = 9
+
+    1 = 5 = 9
+    3 = 5 = 7
+
+  */
+
+  function turno() {
+    //let vteste = [0,0,0, 0,0,0, 0,0,0];
+
+    if (
+      tabuleiro[0] != 0 &&
+      tabuleiro[0] == tabuleiro[1] &&
+      tabuleiro[1] == tabuleiro[2]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[1]);
+      setVencedor(tabuleiro[1]);
+    }
+
+    if (
+      tabuleiro[3] != 0 &&
+      tabuleiro[3] == tabuleiro[4] &&
+      tabuleiro[4] == tabuleiro[5]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[3]);
+      setVencedor(tabuleiro[3]);
+    }
+
+    if (
+      tabuleiro[6] != 0 &&
+      tabuleiro[6] == tabuleiro[7] &&
+      tabuleiro[7] == tabuleiro[8]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[6]);
+      setVencedor(tabuleiro[6]);
+    }
+
+    if (
+      tabuleiro[0] != 0 &&
+      tabuleiro[0] == tabuleiro[3] &&
+      tabuleiro[3] == tabuleiro[6]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[0]);
+      setVencedor(tabuleiro[0]);
+    }
+
+    if (
+      tabuleiro[1] != 0 &&
+      tabuleiro[1] == tabuleiro[4] &&
+      tabuleiro[4] == tabuleiro[7]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[1]);
+      setVencedor(tabuleiro[1]);
+    }
+
+    if (
+      tabuleiro[2] != 0 &&
+      tabuleiro[2] == tabuleiro[5] &&
+      tabuleiro[5] == tabuleiro[8]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[2]);
+      setVencedor(tabuleiro[2]);
+    }
+    if (
+      tabuleiro[0] != 0 &&
+      tabuleiro[0] == tabuleiro[4] &&
+      tabuleiro[4] == tabuleiro[8]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[0]);
+      setVencedor(tabuleiro[0]);
+    }
+    if (
+      tabuleiro[2] != 0 &&
+      tabuleiro[2] == tabuleiro[4] &&
+      tabuleiro[4] == tabuleiro[6]
+    ) {
+      console.log("o vencedor é: " + tabuleiro[2]);
+      setVencedor(tabuleiro[2]);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Olá Galera!</Text>
+      {vencedor != null ? (
+        <Text style={styles.infJogador}>O vencedor é o jogador {jogador} </Text>
+      ) : (
+        <Text style={styles.infJogador}>
+          Agora é a vez do jogador {jogador}{" "}
+        </Text>
+      )}
+
+      <Pressable
+        onPress={() => {
+          setTabuleiro([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+          setJogador(1);
+          setVencedor(null);
+        }}
+      >
+        <Text>Novo Jogo</Text>
+      </Pressable>
+
+      <View style={styles.tabuleiro}>
+        <View style={styles.linha}>
+          <Caixa
+            posicao={1}
+            rodaTurno={turno}
+            alteraTabuleiro={setTabuleiro}
+            tabuleiro={tabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+          <Caixa
+            posicao={2}
+            rodaTurno={turno}
+            alteraTabuleiro={setTabuleiro}
+            tabuleiro={tabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+          <Caixa
+            posicao={3}
+            rodaTurno={turno}
+            alteraTabuleiro={setTabuleiro}
+            tabuleiro={tabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+        </View>
+
+        <View style={styles.linha}>
+          <Caixa
+            posicao={4}
+            rodaTurno={turno}
+            alteraTabuleiro={setTabuleiro}
+            tabuleiro={tabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+          <Caixa
+            posicao={5}
+            rodaTurno={turno}
+            tabuleiro={tabuleiro}
+            jogador={jogador}
+            alteraTabuleiro={setTabuleiro}
+            trocaJogador={setJogador}
+          />
+          <Caixa
+            posicao={6}
+            rodaTurno={turno}
+            tabuleiro={tabuleiro}
+            alteraTabuleiro={setTabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+        </View>
+
+        <View style={styles.linha}>
+          <Caixa
+            posicao={7}
+            rodaTurno={turno}
+            tabuleiro={tabuleiro}
+            alteraTabuleiro={setTabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+          <Caixa
+            posicao={8}
+            rodaTurno={turno}
+            tabuleiro={tabuleiro}
+            alteraTabuleiro={setTabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+          <Caixa
+            posicao={9}
+            rodaTurno={turno}
+            tabuleiro={tabuleiro}
+            alteraTabuleiro={setTabuleiro}
+            jogador={jogador}
+            trocaJogador={setJogador}
+          />
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +212,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
+  },
+
+  tabuleiro: {},
+
+  linha: {
+    flexDirection: "row",
+  },
+
+  infJogador: {
+    fontSize: 22,
   },
 });
---
+++ Caixa.js
@@ -0,0 +1,64 @@
+import { View, StyleSheet, Pressable } from "react-native";
+
+import Entypo from "@expo/vector-icons/Entypo";
+import { useState } from "react";
+
+export default function Caixa(props) {
+  let ind = props.posicao - 1;
+  //const [jogador, setJogador] = useState(0);
+  const jogador = props.tabuleiro[ind];
+
+  let jogada = null;
+
+  if (jogador == 1) {
+    jogada = <Entypo name="cross" size={72} />;
+  } else if (jogador == 2) {
+    jogada = <Entypo name="circle" size={72} />;
+  }
+
+  function jogar() {
+    let ind = props.posicao - 1;
+
+    if (jogador == 0) {
+      //setJogador(props.jogador);
+
+      // por causa da farma que o react trabalha com
+      // array em variaveis state
+      let tabuleiro = [...props.tabuleiro];
+      tabuleiro.splice(ind, 1, props.jogador);
+
+      props.alteraTabuleiro([...tabuleiro]);
+
+      let proximo = props.jogador == 1 ? 2 : 1;
+      props.trocaJogador(proximo);
+
+      console.log(tabuleiro, props.posicao);
+
+      props.rodaTurno();
+    }
+  }
+
+  return (
+    <View style={css.caixa}>
+      <Pressable onPress={jogar} style={css.botao}>
+        {jogada}
+      </Pressable>
+    </View>
+  );
+}
+
+const css = StyleSheet.create({
+  caixa: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderStyle: "solid",
+    height: 100,
+    width: 100,
+  },
+  botao: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 100,
+    width: 100,
+  },
+});
